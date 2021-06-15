cba318877fd4e55a5e8b3b20eaa032abc416f4cd
vairakkani <vpathamuthu@endosoft.com>
2021-06-15 19:28:37 +0530

    added project template files for web pages

--- crud_operations/views.py
+++ crud_operations/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,'index.html')
+
+def create(request):
+    return render(request,'create.html')
+
+def update(request):
+    return render(request,'update.html')
+
+def delete(request):
+    return render(request,'delete.html')
